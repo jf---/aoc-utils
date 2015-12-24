e55ab9ae0526fadd8fce88ca5ba763ab68964f3a
floatingpointstack <floatingpoint.development@gmail.com>
2015-12-24 16:15:38 +0100

    API : option to use the minimum dimension of the bounding box to compute linear deflection in mesh.py

--- aocutils/mesh.py
+++ aocutils/mesh.py
@@ -5,7 +5,14 @@ r"""mesh.py
 
 Summary
 -------
-Mashing logic for shapes, based on shape dimensions
+Meshing logic for shapes, based on shape dimensions
+
+Notes
+-----
+
+BRepMesh_IncrementalMesh (const TopoDS_Shape &theShape, const Standard_Real theLinDeflection,
+                          const Standard_Boolean isRelative=Standard_False, const Standard_Real theAngDeflection=0.5,
+                          const Standard_Boolean isInParallel=Standard_False)
 """
 
 import OCC.BRepMesh
@@ -13,7 +20,7 @@ import OCC.BRepMesh
 import aocutils.analyze.bounds
 
 
-def mesh(shape, factor=4000.):
+def mesh(shape, factor=4000., use_min_dim=False):
     r"""Mesh a shape
 
     Parameters
@@ -22,8 +29,16 @@ def mesh(shape, factor=4000.):
         Shape to mesh
     factor : float
         The higher, the finer the mesh
+    use_min_dim : bool (optional)
+        Use minimum bounding box dimension to compute the linear deflection.
+        The default is False (i.e. use max dimension)
+        This is useful for long and thin objects where using the max dimension would result in a very coarse linear
+        deflection in the other directions.
 
     """
     bb = aocutils.analyze.bounds.BoundingBox(shape)
-    OCC.BRepMesh.BRepMesh_IncrementalMesh(shape, bb.max_dimension / factor)
+    if use_min_dim:
+        OCC.BRepMesh.BRepMesh_IncrementalMesh(shape, bb.min_dimension / factor)
+    else:
+        OCC.BRepMesh.BRepMesh_IncrementalMesh(shape, bb.max_dimension / factor)
     # return shape
