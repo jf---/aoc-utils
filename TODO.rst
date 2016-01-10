a206f0605d7efcdc477206b2fbd44eb903663550
floatingpointstack <floatingpoint.development@gmail.com>
2016-01-10 20:44:06 +0100

    API : added cylinder and tube to primitives.py

--- TODO.rst
+++ TODO.rst
@@ -2,8 +2,10 @@
 General
 -------
 -> pypi (see http://peterdowns.com/posts/first-time-with-pypi.html)
+    DELETE FROM PYPI -> MOVE TO ANACONDA (make sure all dependencies are included in the recipe)
 -> CI
 -> Coverage
+it is a library, easy on logging (debug for almost everything, info if very important)
 
 tests
 -----
@@ -16,6 +18,11 @@ Python 3 tests
 analyze
 -------
 global properties of compound
+
+operations/boolean
+------------------
+Volume / area of compounds -> what is current behaviour? what is desired behaviour?
+cleaner boolean functions
 
 examples
 --------
--- aocutils/primitives.py
+++ aocutils/primitives.py
@@ -10,6 +10,8 @@ Simple shapes creation: box, sphere etc ....
 
 """
 
+from __future__ import division
+
 import functools
 
 import OCC.BRepPrimAPI
@@ -32,7 +34,7 @@ def box(*args):
     """
     a_box = OCC.BRepPrimAPI.BRepPrimAPI_MakeBox(*args)
     a_box.Build()
-    with aocutils.common.AssertIsDone(a_box, 'failed to built a cube...'):
+    with aocutils.common.AssertIsDone(a_box, 'failed to build a cube...'):
         return a_box.Shape()
 
 
@@ -51,5 +53,53 @@ def sphere(*args):
     """
     a_sphere = OCC.BRepPrimAPI.BRepPrimAPI_MakeSphere(*args)
     a_sphere.Build()
-    with aocutils.common.AssertIsDone(a_sphere, 'failed to built a sphere...'):
+    with aocutils.common.AssertIsDone(a_sphere, 'failed to build a sphere...'):
         return a_sphere.Shape()
+
+
+@functools.wraps(OCC.BRepPrimAPI.BRepPrimAPI_MakeCylinder)
+def cylinder(*args):
+    r"""Make a cylinder
+
+    Parameters
+    ----------
+    args
+
+    Returns
+    -------
+    OCC.TopoDS.TopoDS_Shape
+
+    """
+    a_cylinder = OCC.BRepPrimAPI.BRepPrimAPI_MakeCylinder(*args)
+    a_cylinder.Build()
+    with aocutils.common.AssertIsDone(a_cylinder, 'failed to build a cylinder...'):
+        return a_cylinder.Shape()
+
+
+def tube(outer_diameter, inner_diameter, length):
+    r"""Make a tube
+
+    Parameters
+    ----------
+    outer_diameter
+    inner_diameter
+    length
+
+    Returns
+    -------
+    OCC.TopoDS.TopoDS_Shape
+
+    """
+    import aocutils.operations.boolean
+
+    out_cylinder = OCC.BRepPrimAPI.BRepPrimAPI_MakeCylinder(outer_diameter / 2., length)
+    out_cylinder.Build()
+    with aocutils.common.AssertIsDone(out_cylinder, 'failed to build the outer cylinder...'):
+        shape_a = out_cylinder.Shape()
+
+    in_cylinder = OCC.BRepPrimAPI.BRepPrimAPI_MakeCylinder(inner_diameter / 2., length)
+    in_cylinder.Build()
+    with aocutils.common.AssertIsDone(in_cylinder, 'failed to build the inner cylinder...'):
+        shape_b = in_cylinder.Shape()
+
+    return aocutils.operations.boolean.cut(shape_a, shape_b)
